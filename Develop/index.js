476e3d068eee8b3e589009b30478ebcc1b1173d5
mushroom <shaelah.mai@gmail.com>
2023-02-15 18:59:12 +1000

    added to generating js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions =
@@ -57,16 +57,10 @@ const questions =
         },
     ])
 
-// TODO: Create a function to write README file
-function writeToFile(filename, data) {
-    fs.writeFileSync(filename, generateMarkdown(data))
-};
+.then((answers) => {
+    const readmePageContent = generateMarkdown(answers);
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then(data => {writeToFile('README.md', data)
-    })
-};
-
-// Function call to initialize app
-// init();
+    fs.writeFile('README.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
