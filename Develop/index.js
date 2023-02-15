c6695116b3011b054ffcccccc402e736e55a35bd
mushroom <shaelah.mai@gmail.com>
2023-02-15 13:41:53 +1000

    index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,12 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+// const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
+const questions =
+    inquirer
+    .prompt([
         {
             type: 'input',
             name:'github',
@@ -26,8 +29,8 @@ const questions = () => {
         },
         {
             type: 'list',
-            name:'licence',
-            message: 'What kind of licence would you liek your project to have?',
+            name:'license',
+            message: 'What kind of licence would you like your project to have?',
             choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
         },
         {
@@ -52,17 +55,18 @@ const questions = () => {
             name:'contribute',
             message: 'What does the user need to know about contributing to the repo?',
         },
-    ]);
-};
-
-const generateReadme = ({github, email, project, descrip, licence, depen, test, use, contribute}) =>
-'# ${project} '
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {
+    fs.writeFileSync(filename, generateMarkdown(data))
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => {writeToFile('README.md', data)
+    })
+};
 
 // Function call to initialize app
-init();
+// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,9 +12,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project}
 
-`;
-}
+  ##Licence
+
+  ##Description
+  ${data.decrip}
+
+  ##Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ##Installation
+  To install the necessary dependencies, run the following command:
+  '${data.depen}'
+
+  ##Usage
+  
+  ##License
+  This project is licensed under the ${data.license}
+
+  ##Contributing
+  If you wish to contribute to the project please email me at ${data.email}
+
+  ##Tests
+  To run tests run the following command:
+  '${data.email}
+
+  ##Questions
+  If you ahve any questions please email me at ${data.email}.
+  Otherwise you can see my other work on my Github profite at <a href="https://github.com/${data.username}"></a>
+`
+};
 
 module.exports = generateMarkdown;
